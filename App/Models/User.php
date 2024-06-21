5174d03bc2857645c1e7c14b54ce7eca57864b54
aminmhd <amin.m.abarashi@gmail.com>
2024-06-21 18:43:32 +0200

    improving the user model

--- App/Models/User.php
+++ App/Models/User.php
@@ -7,15 +7,6 @@ use PDO;
 
 
 class User extends Model {
-    protected static $db;
-    
-
-   public function __construct(){
-      self::$db = parent::$conn;
-   }   
-   public static function update(){
-     
-   }
+  
    
-
 }
